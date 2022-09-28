5522835abb9e382f41b68b9f85367728eb7cfd9a
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-09-28 00:10:11 -0300

    lint errors corrected

--- src/App.js
+++ src/App.js
@@ -2,37 +2,12 @@ import React from 'react';
 import Form from './components/Form';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      cardName: '',
-      cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
-      cardImage: '',
-      cardRare: '',
-      hasTrunfo: false,
-      isSaveButtonDisabled: false,
-    };
-  }
-
-  handleInputChange() {
-    return 'oi';
-  }
-
-  handleSaveButtonClick() {
-    return 'tchau';
-  }
-
   render() {
     return (
-      <div>
+      <>
         <h1>Tryunfo</h1>
-        <Form
-          state={ this.state }
-        />
-      </div>
+        <Form />
+      </>
     );
   }
 }
